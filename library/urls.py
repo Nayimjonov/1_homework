6a5305c50368e578ee508eae3c7eeba263eb397c
Samandar <samandarnayimjonov@gmail.com>
2025-02-20 15:04:00 +0200

    url tugrlandi

--- library/urls.py
+++ library/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns=[
     path('books/', views.BookListCreateView.as_view(), name='list'),
-    path('detail/<int:pk>/', views.BookDetailView.as_view(), name='detail'),
+    path('books/<int:pk>/', views.BookDetailView.as_view(), name='detail'),
 ]
